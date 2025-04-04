011380097d7636755f18d5f96a7363b05a056471
Omar <omarelhosany3@gmail.com>
2025-04-04 15:29:58 +0200

    fixing to call with filename not with the same filename everytime

--- src/visualizer/draw_dfa.py
+++ src/visualizer/draw_dfa.py
@@ -1,7 +1,7 @@
 import graphviz
 from collections import deque
 
-def draw_dfa(dfa):
+def draw_dfa(dfa, filename='dfa_graph'):
     dot = graphviz.Digraph('DFA')
     dot.attr(rankdir='LR')
     
@@ -51,4 +51,4 @@ def draw_dfa(dfa):
     print("DFA graph saved as 'dfa_graph'")
     
     # Also render as PNG for visualization
-    dot.render('dfa_graph', format='png', view=True, cleanup=True)
+    dot.render(filename, format='png', view=True, cleanup=True)  # Will open the image
